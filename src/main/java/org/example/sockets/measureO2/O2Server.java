8c4b93c8a8c11eb1ffa609a074124de5d22158bb
محمد احمد <muhammad.ahmad.github@gmail.com>
2025-12-15 23:09:21 +0500

    ref(sockets): scope issues of sockets in threads

--- src/main/java/org/example/sockets/measureO2/O2Server.java
+++ src/main/java/org/example/sockets/measureO2/O2Server.java
@@ -10,34 +10,45 @@ public class O2Server {
             System.out.println("Server has started!");
 
             while (true) {
-                Socket s = serverSocket.accept();
-                Thread t = new Thread(new Runnable() {
-                    public void run() {
-                        try {
-                            //in
-                            InputStream in = s.getInputStream();
-                            BufferedReader i = new BufferedReader(new InputStreamReader(in));
-
-                            String str;
-                            while ((str = i.readLine()) != null) {
-                                int value = Integer.parseInt(str);
-                                if (value < 80) System.out.println("Patient has Oxygen level less than 80");
-                            }
-                        } catch (IOException e) {
-                        } finally {
-                            try {
-                                s.close();
-                            } catch (IOException e) {
-                            }
-                        }
-                    }
-                }
-                );
-
+                Socket clientSocket = serverSocket.accept();
+                Thread t = new Thread(new ClientHandler(clientSocket));
                 t.start();
             }
 
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ClientHandler implements Runnable {
+        private final Socket socket;
+
+        public ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                InputStream in = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String str;
+                while ((str = reader.readLine()) != null) {
+                    int value = Integer.parseInt(str);
+                    if (value < 80) {
+                        System.out.println("Patient has Oxygen level less than 80");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
